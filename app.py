35bdd239e3c9724be50f2b88b0c4d345d425ae53
andres hermoso <andreshermoso2013@gmail.com>
2024-07-24 19:13:40 -0400

    form 2 agregado

--- app.py
+++ app.py
@@ -5,16 +5,5 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/submit', methods=['POST'])
-def submit():
-    variables = request.form['variables']
-    restricciones = request.form['restricciones']
-    
-    with open('datos.txt', 'w') as f:
-        f.write(f'variable1: {variables}\n')
-        f.write(f'variable2: {restricciones}\n')
-    
-    return 'Datos guardados con éxito'
-
 if __name__ == '__main__':
     app.run(debug=True)
